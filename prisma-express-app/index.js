dfbb6abf8a5e2b5e62d36416b329b7a260c44b63
WooGruvia <vegef40@gmail.com>
2025-12-09 21:50:26 -0300

    certificado- estructurar a https sin key

--- prisma-express-app/index.js
+++ prisma-express-app/index.js
@@ -1,6 +1,15 @@
-const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const app = require('./src/app'); 
 
-app.listen(PORT, () => {
-  console.log(`Servidor Modular corriendo en http://localhost:${PORT}`);
+const PORT = 3443;
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'server.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'server.cert'))
+};
+
+https.createServer(options, app).listen(PORT, () => {
+    console.log(`✅ Servidor Seguro (HTTPS) corriendo en: https://localhost:${PORT}`);
 });
